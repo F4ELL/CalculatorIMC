42ccea8536ec2e681f0530ecc140be825dd6e380
F4ELL <ferreiraperes1234@gmail.com>
2022-02-25 16:10:14 -0300

    feat: create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png'
 
 function App() {
   return (
-    <div className="App">
-      ALOOOOOOOOOOOOO
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150}/>
+        </div>
+      </header>
     </div>
   );
 }
