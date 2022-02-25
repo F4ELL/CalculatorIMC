33214645addbd76c2386d94709fc2c1fd6000edc
F4ELL <ferreiraperes1234@gmail.com>
2022-02-25 18:12:59 -0300

    feat: making work

--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -17,8 +17,10 @@ export const calculateImc = (height: number, weight: number) => {
     const imc = weight / (height * height)
 
     for(let i in levels){
-        if(imc >= levels[i].imc[0] && imc < levels[i].imc[1])
-            return levels[i];
+        if(imc >= levels[i].imc[0] && imc < levels[i].imc[1]){
+            levels[i].yourImc = imc
+            return levels[i]
+        }
     }
 
     return null;
