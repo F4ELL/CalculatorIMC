d4889438dbae20b17a875edb06b696d09e4fa9a2
F4ELL <ferreiraperes1234@gmail.com>
2022-02-25 16:30:43 -0300

    feat: create inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
 import styles from './App.module.css';
 import poweredImage from './assets/powered.png'
 
 function App() {
+
+  const [ heightField, setHeightField ] = useState<number>(0)
+  const [ weightField, setWeightField ] = useState<number>(0)
+
+
+
+
   return (
     <div className={styles.main}>
       <header>
@@ -12,7 +19,22 @@ function App() {
       </header>
       <div className={styles.container}>
         <div className={styles.leftSide}>
-          ...
+          <h1>Calcule o seu IMC</h1>
+          <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial da Saúde para calcular o peso ideal de cada pessoa.</p>
+
+          <input 
+            type="number"
+            placeholder="Digite a sua altura. Ex: 1.5 (em metros)" 
+            value={heightField > 0 ? heightField : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}  
+          />
+
+          <input 
+            type="number"
+            placeholder="Digite o seu peso. Ex: 75.3 (em quilos)" 
+            value={weightField > 0 ? weightField : ''}
+            onChange={e => setWeightField(parseFloat(e.target.value))}  
+          />
         </div>
         <div className={styles.rightSide}>
           ...
